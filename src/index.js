87a1b7ff6520ba2fa80f2a606604d7503bc1fa24
ryandefea <ryan.defea@gmail.com>
2022-07-01 11:44:28 -0700

    make async function.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Template from './js/template.js';
+import CurrencyService from './currency-service.js';
+
+
+
+
+
+async function makeApiCall(country, amount) {
+  const response = await CurrencyService.getCurrency(country, amount);
+  getCurrencyInfo(response);
+}
