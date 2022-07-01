79f481f2e3b062260cf91e662aa56e244a5846cd
ryandefea <ryan.defea@gmail.com>
2022-07-01 13:53:10 -0700

    change repos becuase it would not open in browser, something was wrong with webpack or the package json file and i could not figure it out so i started a new repo.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,23 @@ import './css/styles.css';
 import CurrencyService from './currency-service.js';
 
 
+const service = new CurrencyService();
+//function getCurrencyInfo(country, amount) {
+
+//}
 
 
 
 async function makeApiCall(country, amount) {
-  const response = await CurrencyService.getCurrency(country, amount);
-  getCurrencyInfo(response);
+  const response = await service.getCurrency(country, amount);
+  //getCurrencyInfo(response);
+  console.log(response);
 }
 
 $(document).ready(function() {
-  $('#')
+  $('#moneySubmit').click(function () {
+    let amount = parseInt($('#amount').val());
+    let country = $('#country').val();
+    makeApiCall(country, amount);
+  })
 })
